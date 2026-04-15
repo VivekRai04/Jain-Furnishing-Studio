00400c66d42f19d5448de8cc89007ecdb4501e3e
Vivek Rai <raiv5253@gmail.com>
2026-04-16 01:36:19 +0530

    Delete generate-hash.ts

--- generate-hash.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = 'JainStudio2026';
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds).then((hash) => {
-  console.log('Hash:', hash);
-  process.exit(0);
-});
